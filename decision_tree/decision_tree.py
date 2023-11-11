3dc03bf3f3f87d356eccc86b810f15f1c6fcbf99
beongkyuPark <byeonggyu.park.kr@gmail.com>
2023-11-10 23:06:18 -0800

    added comments

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,9 +1,12 @@
+# HW7 : decision tree
+# ByoengKyu Park(byeonggyu.park)
 import numpy as np
 import pandas as pd
 from graphviz import Digraph
+import random
 
-
-# Define the dataset as given in the user's image
+#---------------------------------------------------------------------DATA
+# the dataset
 data = pd.DataFrame({
 	'Age': [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 2],
 	'Prescription': [1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2],
@@ -12,72 +15,67 @@ data = pd.DataFrame({
 	'Label': [3, 2, 3, 1, 3, 2, 3, 1, 3, 2, 3, 1, 3, 2, 3, 3, 3, 3, 3, 1, 3, 2, 3, 2]
 })
 
-# Function to calculate the entropy of the dataset
-def entropy(target_col):
+#---------------------------------------------------------------------HELPER FUNCTIONS
+# calc the entropy
+def calc_entropy(target_col):
     elements, counts = np.unique(target_col, return_counts=True)
     entropy = np.sum([(-counts[i]/np.sum(counts))*np.log2(counts[i]/np.sum(counts)) for i in range(len(elements))])
     return entropy
 
-import random
 
-# Modify the InfoGain function to handle ties randomly
-def InfoGain(data, split_attribute_name, target_name="Label"):
-    total_entropy = entropy(data[target_name])
+# calc the info gain (for ties, pick randomly)
+def cacl_IG(data, split_attribute_name, target_name="Label"):
+    total_entropy = calc_entropy(data[target_name])
     vals, counts = np.unique(data[split_attribute_name], return_counts=True)
-    Weighted_Entropy = np.sum([(counts[i]/np.sum(counts))*entropy(data.where(data[split_attribute_name]==vals[i]).dropna()[target_name]) for i in range(len(vals))])
+    Weighted_Entropy = np.sum([(counts[i]/np.sum(counts))*calc_entropy(data.where(data[split_attribute_name]==vals[i]).dropna()[target_name]) for i in range(len(vals))])
     Information_Gain = total_entropy - Weighted_Entropy
     return Information_Gain
 
-# Modify the ID3 function to pick a random attribute if there's a tie and to list indices of rows at each leaf
+# id3 algorithm
 def ID3(data, originaldata, features, target_attribute_name="Label", parent_node_class = None, depth=0):
-    # If all target_values have the same value, return this value with the indices
+    # if all in the subset have the same label, return the label with the indices
     if len(np.unique(data[target_attribute_name])) <= 1:
         return (np.unique(data[target_attribute_name])[0], data.index.tolist())
     
-    # If the subset is empty, return the most common class of the parent subset
+    # for empty subset, returns the mode label of the parent subset
     elif data.empty:
         return (parent_node_class if parent_node_class is not None else np.unique(originaldata[target_attribute_name])[np.argmax(np.unique(originaldata[target_attribute_name], return_counts=True)[1])], [])
         
-    # If there are no more attributes to consider, return the most common class in the current subset
+    # no more attributes to consider, return the mode label in the current subset
     elif len(features) == 0:
         return (np.unique(data[target_attribute_name])[np.argmax(np.unique(data[target_attribute_name], return_counts=True)[1])], data.index.tolist())
-    
-    
-    # If none of the above conditions hold true, grow the tree!
+        
+    # building branches
     else:
-        # Set the default value for this node --> The mode target feature value of the current node
+        # the default value for this node is the mode target feature value of the current node
         parent_node_class = np.unique(data[target_attribute_name])[np.argmax(np.unique(data[target_attribute_name], return_counts=True)[1])]
         
-        # Select the feature which best splits the dataset
-        item_values = [InfoGain(data, feature, target_attribute_name) for feature in features] # Return the information gain values for the features in the dataset
+        # decide the feature which yields the highest IG (=lowest entropy)
+        item_values = [cacl_IG(data, feature, target_attribute_name) for feature in features] # Return the information gain values for the features in the dataset
         
-        # Handle ties in information gain by picking a random attribute
+        # picks a random attribute for ties
         max_gain = max(item_values)
         best_features = [features[i] for i, gain in enumerate(item_values) if gain == max_gain]
         best_feature = random.choice(best_features) if len(best_features) > 1 else best_features[0]
         
-        # Create the tree structure
         tree = {best_feature:{}}
         
-        # Remove the feature with the best information gain from the feature space
+        # extract remaining features
         features = [i for i in features if i != best_feature]
         
-        # Grow a branch under the root node for each possible value of the root node feature
+        # Grow a branch 
         for value in np.unique(data[best_feature]):
             sub_data = data.where(data[best_feature] == value).dropna()
             
-            # Call the ID3 algorithm recursively
+            # recursive call for subtrees
             subtree = ID3(sub_data, originaldata, features, target_attribute_name, parent_node_class, depth+1)
             
-            # Add the sub tree to the tree under the root node
+            # add the subtree to the tree under the root node
             tree[best_feature][value] = subtree
             
         return(tree)
 
-# Run the modified ID3
-tree = ID3(data, data, data.columns[:-1])
-print(tree)
-
+#---------------------------------------------------------------------PLOTTING FUNCTION
 def plot_tree(graph, tree, parent_name, counter=0):
     # Base case
     if type(tree) == tuple:
@@ -99,13 +97,17 @@ def plot_tree(graph, tree, parent_name, counter=0):
             counter = plot_tree(graph, child_tree, child_name, counter)
 
     return counter
+#--------------------------------------------------------------------- MAIN
+# (1) build the tree
+tree = ID3(data, data, data.columns[:-1])
+print(tree)
 
-# Initialize graph object
+# init
 dot = Digraph(comment='Decision Tree')
 
-# Plot the decision tree
+# (2-1) plot the decision tree
 plot_tree(dot, tree, '', 0)
 
-# Save and render the graph to an output file
+# (2-2) Save and render the graph to an output file
 dot.render('C:/math345/decision_tree', view=True, format='png')
 
