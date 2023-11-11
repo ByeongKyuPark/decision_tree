960296787cfdce10da5f9a49906c758f29a4033f
beongkyuPark <byeonggyu.park.kr@gmail.com>
2023-11-11 07:16:02 -0800

    hw7_2_(1), (working)

--- decision_tree/k_cluster.py
+++ decision_tree/k_cluster.py
@@ -1,64 +1,109 @@
+#HW7-2-(1) Use the k-means algorithm to cluster the data into 2 clusters. Start with centroids m1 = (1; 1; 0) and
+#          m2 = (2; 3; 0). Output centroids and clusters.
+# ByeongKyu Park (byeonggyu.park)
+
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# Given dataset D
-D = np.array([[-1, 1], [-1, 2], [0, 1], [1, 1], [2, 2], [2, 4]])
+# dataset
+D = np.array([
+    [1, 1, 0],
+    [1, 0, 1],
+    [2, 0, 0],
+    [1, 3, 1],
+    [3, 3, 2],
+    [3, 0, 0],
+    [0, 2, 1],
+    [2, 3, 0],
+    [0, 0, 0],
+    [2, 1, 3]
+])
 
-# Number of clusters
+# the number of clusters
 k = 2
 
-# Function to initialize centroids
+# init centroids(has not been used for this homework)
 def initialize_centroids(points, k):
     # Randomly choose the first centroid
     centroids = [points[np.random.choice(len(points))]]
     for _ in range(1, k):
-        # Calculate distances from the already chosen centroids
+        # clac distances from the chosen centroids
         distances = np.sqrt(((points - np.array(centroids))**2).sum(axis=1))
-        # Find the data point with the maximum distance to all existing centroids
-        # For ties, np.random.choice will pick one index randomly
+        # pick a point with the max dist to all existing centroids (for ties, pick randomly)
         furthest = np.random.choice(np.where(distances == np.max(distances))[0])
         centroids.append(points[furthest])
     return np.array(centroids)
 
-# Initialize centroids
-centroids = initialize_centroids(D, k)
-centroids = np.array([[-1,1],[1,1]])
+# initialize centroids
+#centroids = initialize_centroids(D, k)
+centroids = np.array([[1, 1, 0], [2, 3, 0]])
 
-# Function to assign points to the nearest centroid
+# assign nearest centroid
 def assign_points_to_centroids(points, centroids):
     distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
-    # If there's a tie, pick a random centroid from the ones with minimum distance
+    # for a tie, pick a random centroid among min dists
     closest_centroid = np.array([np.random.choice(np.flatnonzero(dist == dist.min())) for dist in distances.T])
     return closest_centroid
 
-# Function to update centroids
+# compute the centroids
 def update_centroids(points, assignments, k):
     new_centroids = np.array([points[assignments == i].mean(axis=0) for i in range(k)])
     return new_centroids
 
-# Function to perform k-means clustering
+# k-means clustering
 def k_means(points, centroids, k):
     converged = False
     while not converged:
-        # Step 2: Assign points to the nearest centroid
+        # decide labels of each point
         closest_centroid = assign_points_to_centroids(points, centroids)
         
-        # Step 3: Recompute the centroids
+        # recompute the centroids
         new_centroids = update_centroids(points, closest_centroid, k)
         
-        # Check for convergence
+        # converging ?
         converged = np.all(centroids == new_centroids)
         centroids = new_centroids
     
     return centroids, closest_centroid
 
-# Run k-means clustering
+# run k-means clustering
 final_centroids, closest_centroids = k_means(D, centroids, k)
 
-# Plotting the clusters
-plt.scatter(D[:, 0], D[:, 1], c=closest_centroids, s=50, cmap='viridis')
-plt.scatter(final_centroids[:, 0], final_centroids[:, 1], c='red', s=100, marker='x')
-plt.title('2-Means Clustering')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
+# prints the final centroids
+print("Centroids: ")
+print(final_centroids)
+print()
+
+#prints labels of each point
+print("Labels: ")
+cluster_labels = ['Cluster {}'.format(i) for i in closest_centroids]
+print(cluster_labels)
+
+
+# 3d scatter plotting with grid lines
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# data points
+scatter = ax.scatter(D[:, 0], D[:, 1], D[:, 2], c=closest_centroids, s=50, cmap='viridis', depthshade=False)
+
+# centroids
+ax.scatter(final_centroids[:, 0], final_centroids[:, 1], final_centroids[:, 2], c='red', s=100, marker='x')
+
+# grids (just for visibilities)
+for i in range(D.shape[0]):
+    ax.plot([D[i,0], D[i,0]], [D[i,1], D[i,1]], [0, D[i,2]], '--', linewidth=0.5, color='grey', alpha=0.7)
+    ax.plot([D[i,0], D[i,0]], [0, D[i,1]], [D[i,2], D[i,2]], '--', linewidth=0.5, color='grey', alpha=0.7)
+    ax.plot([0, D[i,0]], [D[i,1], D[i,1]], [D[i,2], D[i,2]], '--', linewidth=0.5, color='grey', alpha=0.7)
+
+# labels
+ax.set_xlabel('X1')
+ax.set_ylabel('X2')
+ax.set_zlabel('X3')
+
+# title
+ax.set_title('2-Means Clustering (with Grid Lines)')
+
+# plotting
 plt.show()
